c0f780903f98cc73b88f068b8dded0be2ebd28ea
Thanmai BK <techstation09@gmail.com>
2020-12-10 01:52:34 +0530

    ML AI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+import React,{useState} from "react";
+import ReactMapGl ,{Marker, Popup} from "react-map-gl";
+import * as UniData from './csvjson.json';
+
+export default function  App(){
+  const [viewport,setviewport] = useState({
+    latitude : 20.5937,
+    longitude : 78.9629,
+    zoom : 4,
+    width: '100vw',
+    height :'100vh'
+  })
+ const [selectedPark, setSelectedPark] = useState(null);
+  return (<div>
+    <ReactMapGl {...viewport} mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+   // mapStyle = 'mapbox://styles/thanmaibk1999/ckiht634v6aft19qrsaxggctv'
+    onViewportChange={viewport => {
+      setviewport(viewport);
+      }}>
+      {UniData.features.map((park)=>(
+        <Marker key={park.properties.PARK_ID} latitude ={park.geometry.coordinates[1]}
+        longitude={park.geometry.coordinates[0]}>
+          <button className = 'marker-btn' onClick ={(e)=>{
+            e.preventDefault();
+            setSelectedPark(park);
+
+
+          }}>
+            <img src ='/apartment-building.svg' alt='University'/>
+          </button>
+        </Marker>
+
+      ))}
+
+      { selectedPark ? (
+        <Popup latitude= {selectedPark.geometry.coordinates[1]}
+        longitude={selectedPark.geometry.coordinates[0]}
+        onClose={ () => {
+          setSelectedPark(null);
+        }
+        }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div>
+            University
+          </div>
+        </Popup>
+      ) : null}
+    </ReactMapGl>
+  </div>
   );
 }
-
-export default App;
